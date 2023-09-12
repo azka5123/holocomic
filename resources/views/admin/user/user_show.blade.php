ad628c518de066b47866e95fe90fb4b3bff3144a
azka5123 <azka.thoyyib@gmail.com>
2023-09-12 22:58:43 +0700

    pembuatan table pada folder user di file user_show

--- resources/views/admin/user/user_show.blade.php
+++ resources/views/admin/user/user_show.blade.php
@@ -5,32 +5,31 @@
 
 @section('main_content')
     <div class="card-body">
-        <table id="example1" class="table table-bordered table-striped">
+        <table id="example1" class="table table-bordered table-striped " width="100%" cellspacing="10">
             <thead>
-                <tr>
-                    <th>Rendering engine</th>
-                    <th>Browser</th>
-                    <th>Platform(s)</th>
-                    <th>Engine version</th>
-                    <th>CSS grade</th>
+                <tr class="text-center">
+                    <th>Username</th>
+                    <th>Email</th>
+                    <th>action</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
                     <td>Trident</td>
                     <td>Internet Explorer 4.0</td>
-                    <td>Win 95+</td>
-                    <td>4</td>
-                    <td>X</td>
+                    <td class="">
+                        <a href="#" class="btn btn-sm btn-primary px-3 "> <ion-icon name="trash-outline"></ion-icon>
+                            Banned</a>
+                        <a href="#" class="btn btn-sm btn-primary px-3 "> <ion-icon name="trash-outline"></ion-icon>
+                            Delete</a>
+                    </td>
                 </tr>
             </tbody>
             <tfoot>
-                <tr>
-                    <th>Rendering engine</th>
-                    <th>Browser</th>
-                    <th>Platform(s)</th>
-                    <th>Engine version</th>
-                    <th>CSS grade</th>
+                <tr class="text-center">
+                    <th>Username</th>
+                    <th>Email</th>
+                    <th>action</th>
                 </tr>
             </tfoot>
         </table>
